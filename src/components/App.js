cdd9d40855433018230a9c65e22e3539ac563fb3
mfatekhov <brutalduck@gmail.com>
2020-03-19 23:22:47 +0300

    Added routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,9 @@
 import React, {Component} from 'react';
 import PlayersList from "./PlayersList";
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import Home from "./Home";
+import NewGame from "./NewGame";
+
 
 class App extends Component {
     constructor(props) {
@@ -48,14 +52,31 @@ class App extends Component {
     render() {
         let title = 'Players';
         return (
-            <div className="container">
-                <div className="row">
-                    <PlayersList title={title} players={this.state.players}/>
+            // <div className="container">
+            //     <div className="row">
+            //         <PlayersList title={title} players={this.state.players}/>
+            //     </div>
+            //     <button onClick={this.addPlayer}>
+            //         Add
+            //     </button>
+            // </div>
+            <Router>
+                <div>
+                    <h2>Welcome to React Router Tutorial</h2>
+                    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                        <ul className="navbar-nav mr-auto">
+                            <li><Link to={'/'} className="nav-link"> Home </Link></li>
+                            <li><Link to={'/new'} className="nav-link"> New Game </Link></li>
+                        </ul>
+                    </nav>
+                    <hr/>
+                    <Switch>
+                        <Route exact path='/' component={Home}/>
+                        <Route path='/new' component={NewGame}/>
+                    </Switch>
                 </div>
-                <button onClick={this.addPlayer}>
-                    Add
-                </button>
-            </div>
+            </Router>
+
         );
     }
 }
---
+++ src/components/NewGame.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class NewGame extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Start New Game</h2>
+            </div>
+        )
+    }
+}
+
+export default NewGame;
